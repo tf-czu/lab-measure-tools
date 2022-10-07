28632e048bd60103849c32554d99a43243f52acc
tajgr <jlev@tf.czu.cz>
2022-10-07 14:48:32 +0200

    apple_pendulum/pendulum.py: add version for first real measur

--- apple_pendulum/pendulum.py
+++ apple_pendulum/pendulum.py
@@ -19,7 +19,24 @@ g_rate = "44100"  # 96000
 g_arecord_command = ["arecord", "-r", g_rate, "tmp/test.wav"]
 g_mic_recorder = None
 g_measure_label = ""
+g_num_repetitions = 2
+g_run = 1
+g_data_list = []
 
+
+def adjust_data(data_list):
+    ret_list = []
+    max_len = 0
+    for data in data_list:
+        if len(data) > max_len:
+            max_len = len(data)
+            
+    for data in data_list:
+        diff = max_len - len(data)
+        ret_list.append(data+[""]*diff)
+        
+    return ret_list
+        
 
 
 class Pendulum:
@@ -54,17 +71,24 @@ def parse_rawdata(rawdata):
             ret.append(int(num))
     return ret
 
-def show_data(data):
-    plt.plot(data)
+def show_data(data_list):
+    colors = ["r", "g", "b", "k", "y"]
+    assert len(data_list) <= len(colors)
+    for ii, data in enumerate(data_list):
+        numbers = [n for n in data if n != ""]
+        plt.plot(numbers, colors[ii], label = f"run_{ii+1}")
+        
+    plt.legend()
     plt.show()
 
 def onButton_record(buttonName):
     global g_mic_recorder
     global g_measure_label
+    global g_run
     command = g_arecord_command
     g_measure_label = app.getEntry("label")
-    print(g_measure_label)
-    wav_name = datetime.datetime.now().strftime(f"apple_{g_measure_label}_%H%M%S.wav")
+    print(g_measure_label, g_run)
+    wav_name = datetime.datetime.now().strftime(f"apple_{g_measure_label}_{g_run}_%H%M%S.wav")
     wav_path = os.path.join("wav", wav_name)
     
     pendulum.stop = False
@@ -74,40 +98,57 @@ def onButton_record(buttonName):
         g_mic_recorder = Launch(command)
         
     time.sleep(1)
-    app.setMessage("mess", f"Recording..\n{g_measure_label}")
+    app.setMessage("mess", f"Recording..\n{g_measure_label}\n{g_run}")
     
     Thread(target=pendulum.record, daemon=True).start()
     g_mic_recorder.start()
 
 
 def onButton_stop(buttonName):
+    global g_data_list
+    global g_run
+    global g_mic_recorder
     pendulum.stop = True
     g_mic_recorder.quit()
+    g_mic_recorder = None
     app.setMessage("mess", "")
     time.sleep(1)
 #    print(pendulum.data)
 #    print(pendulum.ser)
     if len(pendulum.buf) > 0:
         data = parse_rawdata(pendulum.buf)
-#        print(data)
-        csv_name = datetime.datetime.now().strftime(os.path.join("data",f"pendulum_{g_measure_label}_%y%m%d_%H%M%S.csv"))
-        with open(csv_name, "w", newline="") as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=",")
-            for num in data:
-               csv_writer.writerow([num])
-        show_data(data)
-        pendulum.buf = b""
+        g_data_list.append(data)
+        if g_run < g_num_repetitions:
+            g_run += 1
+            pendulum.buf = b""
+        
+        else:
+            data_list = adjust_data(g_data_list)
+            csv_name = datetime.datetime.now().strftime(os.path.join("data",f"pendulum_{g_measure_label}_%y%m%d_%H%M%S.csv"))
+            with open(csv_name, "w", newline="") as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=",")
+                for row in zip(*data_list):
+                   csv_writer.writerow(row)
+            show_data(data_list)
+            pendulum.buf = b""
+            g_run = 1
+            g_data_list = []
+    else:
+        print("NO DATA!")
         
     return
 
-pendulum = Pendulum()
-app = gui()
-#app.setSticky("news")
 
-app.setResizable(canResize=False)
-app.addButton("Record", onButton_record, row=1, column=0)
-app.addButton("Stop", onButton_stop, row=1, column=1)
-app.addEntry("label", row=2)
-app.addMessage("mess", "", row=3, column=0, colspan=2)
-app.go()
+if __name__ == "__main__":
+
+    pendulum = Pendulum()
+    app = gui()
+    #app.setSticky("news")
+
+    app.setResizable(canResize=False)
+    app.addButton("Record", onButton_record, row=1, column=0)
+    app.addButton("Stop", onButton_stop, row=1, column=1)
+    app.addEntry("label", row=2)
+    app.addMessage("mess", "", row=3, column=0)
+    app.go()
 
