8ec8f8a0a6cf6c0a41e944a14a31fdd5df47f225
tajgr <levjakub@gmail.com>
2023-01-31 12:53:24 +0100

    LCR_remote_control.py: Improve code structure, use csv instead of txt in soil

--- LCR_gwinstek/LCR_remote_control.py
+++ LCR_gwinstek/LCR_remote_control.py
@@ -10,33 +10,32 @@ import datetime
 import csv
 
 
-def get_log_path(label):
+def get_log_path(label, prefix=""):
     log_dir = datetime.datetime.now().strftime("logs/lcr_%y%m%d")
     if not os.path.isdir(log_dir):
         os.mkdir(log_dir)
-    timeID = datetime.datetime.now().strftime("_%H%M%S")
+    time_id = datetime.datetime.now().strftime("%H%M%S")
 
-    return os.path.join(log_dir, f"{label}_{timeID}.csv")
+    return os.path.join(log_dir, f"{prefix}{label}_{time_id}.csv")
 
 
 class LCR_8110G:
-    def __init__(self, port, rate = 9600, read_timeout = 5):
+    def __init__(self, port, rate = 9600, read_timeout=5):
         self.read_timeout = read_timeout
         self.major_funcs = [ "C", "L", "X", "B", "Z", "Y" ]
         self.minor_funcs = [ "Q", "D", "R", "G" ]
-        self.ser = serial.Serial(port, rate, parity = serial.PARITY_NONE, timeout = 0.05)
+        self.ser = serial.Serial(port, rate, parity = serial.PARITY_NONE, timeout=0.1)
         assert self.ser.isOpen()
         time.sleep(0.5)
         output = self.sent_cmd("*idn?")
         assert "GW INSTEK" in output, f"LCR is not connected: {output}"
 
-
-    def sent_cmd(self, cmd):
+    def sent_cmd(self, cmd, read_response=True):
         cmd = cmd+"\n"
         self.ser.write(cmd.encode())
-
-        return self.ser_read()
-
+        if read_response:
+            return self.ser_read()
+        return
 
     def ser_read(self):
         t0 = time.time()
@@ -50,12 +49,10 @@ class LCR_8110G:
             if time.time() - t0 > self.read_timeout:
                 return None
 
-
     def set_freq(self, freq):
         assert type(freq) == str, freq
         cmd = f":meas:freq {freq}"
-        return self.sent_cmd(cmd)
-
+        return self.sent_cmd(cmd, read_response=False)
 
     def set_func(self, major, minor = None):
         assert type(major) == str, major
@@ -64,9 +61,7 @@ class LCR_8110G:
             cmd = ":meas:func:"+major+";"+minor
         else:
             cmd = ":meas:func:"+major
-
-        return self.sent_cmd(cmd)
-
+        return self.sent_cmd(cmd, read_response=False)
 
     def read_func(self):
         major_func = None
@@ -79,9 +74,7 @@ class LCR_8110G:
         minor = self.sent_cmd(cmd)
         if minor is not None:
             minor_func = self.minor_funcs[int(minor)]
-
         return major_func, minor_func
-
 
     def read_freq(self):
         cmd = ":meas:freq?"
@@ -90,7 +83,6 @@ class LCR_8110G:
             return float(output)
         return None
 
-
     def trig(self):
         cmd = ":meas:trig"
         output = self.sent_cmd(cmd)
@@ -98,7 +90,6 @@ class LCR_8110G:
             output = output.split(',')
             return [ float(x) for x in output ]
         return None
-
 
     def close(self):
         self.ser.close()
@@ -113,30 +104,23 @@ class LCR_8110G:
 
 def soilSample(label, port):
     freq_list = [1e5, 2e5, 5e5, 1e6, 2e6, 5e6]
-    #freq_list = [1e4]
-    logDir = datetime.datetime.now().strftime("logs/soil_%y%m%d")
-    if not os.path.isdir(logDir):
-        os.mkdir(logDir)
-    timeID = datetime.datetime.now().strftime("_%H%M%S")
-    log_fileN = os.path.join(logDir, "soil_"+label+timeID+".txt")
-    log = open(log_fileN, "w")
+    log_path = get_log_path(label, prefix = "soil_")
 
-    # LCR seting
-    lcr = LCR_8110G(port)
-    log.write("Frequency [Hz], Capacitance [F], Dissipation factor [-], Impedance [ohm], Angle [deg]\r\n")
-    log.flush()
+    with LCR_8110G(port) as lcr:
+        with open(log_path, "w", newline="") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=",")
+            csv_writer.writerow(["Frequency [Hz]", "Capacitance [F]", "Dissipation factor [-]", "Impedance [ohm]", "Angle [deg]"])
 
-    for f in freq_list:
-        lcr.set_freq(str(f))
-        set_freq = lcr.read_freq()
-        lcr.set_func("c", "d")
-        C, D = lcr.trig()
-        lcr.set_func("z")
-        Z, A = lcr.trig()
-        print("f = %.1E Hz, C = %.4E F, D = %.4E, Z = %.4E, A = %f" %(set_freq, C, D, Z, A) )
-        log.write("%.1E Hz, %.4E, %.4E, %.4E, %f\r\n" %(set_freq, C, D, Z, A) )
-        log.flush()
-    log.close()
+            for f in freq_list:
+                lcr.set_freq(str(f))
+                set_freq = lcr.read_freq()
+                lcr.set_func("c", "d")
+                C, D = lcr.trig()
+                lcr.set_func("z")
+                Z, A = lcr.trig()
+                print("f = %.1E Hz, C = %.4E F, D = %.4E, Z = %.4E, A = %f" %(set_freq, C, D, Z, A))
+                csv_writer.writerow([set_freq, C, D, Z, A])
+
     print("Measurement completed")
 
 
@@ -151,7 +135,9 @@ def continuous_measurement(label, port, freq, major_func, minor_func):
             t0 = time.time()
             while True:
                 data = lcr.trig()
-                csv_writer.writerow([(time.time() - t0)*1000] + data)
+                if data:
+                    print(data)
+                    csv_writer.writerow([(time.time() - t0)*1000] + data)
 
 
 if __name__ == "__main__":
@@ -159,7 +145,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='__doc__')
     parser.add_argument('task', help='Specify a type of measurement - available: soil, continuous')
     parser.add_argument('--port', help='Specify serial port', default="/dev/ttyUSB0")
-    parser.add_argument('--label', help='Specify a measurement label', default="")
+    parser.add_argument('--label', help='Specify a measurement label', default="test")
     parser.add_argument('--freq', help='Specify a measurement frequencies', default="1e5")
     parser.add_argument('--major_func', help='Specify a major function')
     parser.add_argument('--minor_func', help='Specify a minor function')
